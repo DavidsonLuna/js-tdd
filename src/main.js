bc6e9b8a0c3bdc90b599485878262767d5042a59
@cs-dluna <d.ferreira.de.luna@accenture.com>
2019-02-03 22:20:12 -0200

    Add eslintrc

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const a = 3;
+const b = 2;
+const c = 'JHJKHKJHJKHJKHKJHJKHJKHJKHJHHJKHKJHKHKJHJKHKHKJHKJHJKHKJHJKHKHJsfsfsfsfsfsfsfsfsffsfsfsfssKHKJHJKHKJHJKHJ';
+
+function sum() {
+  return a + b + c;
+}
+sum();
